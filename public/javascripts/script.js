27739f01a97868f4ef714e6ede58b77161251e86
j-beckman <beckman.joshua@gmail.com>
2014-03-11 23:37:47 -0500

    Mobile view and tablet view are acceptable

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,6 +15,40 @@
   }
 })(this, this.document);
 
+// Init activators
+(function(window, document){
+  var revealers = document.getElementsByClassName('activator'),
+    length = revealers.length,
+    i = 0;
+  function makeRevealer() {
+    var elems = document.getElementsByClassName(this.dataset.activate),
+      deacts = document.getElementsByClassName(this.dataset.deactivate);
+    for (var i = 0; i < elems.length; i++) {
+      classie.toggle(elems[i], 'active');
+    }
+    for (i = 0; i < deacts.length; i++) {
+      classie.toggle(deacts[i], 'active');
+    }
+  }
+  for (; i < length; i++) {
+    revealers[i].addEventListener('click', makeRevealer, false);
+  }
+})(this, this.document);
+
+// Init toggle-btns
+(function(window, document){
+  var btns = document.getElementsByClassName('toggle-btn'),
+    length = btns.length,
+    i = 0;
+  for (; i < length; i++) {
+    btns[i].addEventListener('click', function(){
+      for(var j = 0; j < length; j++){
+        classie.toggle(btns[j].parentNode, 'active');
+      }
+    }, false);
+  }
+})(this, this.document);
+
 function dismissAlert(elem){
   elem.innerHTML = '';
   document.getElementById('messages').style.display = 'none';
